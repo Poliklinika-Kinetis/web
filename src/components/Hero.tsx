6bb3d9fcbc92b34f5cdb1f937382b1e37eaa4deb
Ivan Lučin <lucin.ivan@gmail.com>
2026-02-03 23:20:39 +0100

    Fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,10 @@ export default function Hero() {
           <h1 className={styles.heroHeading}>KINETIS - Korak naprijed.</h1>
 
           <div className={styles.heroText}>
-            <p>Individualan pristup, jasna dijagnostika i terapija bez "šablona".</p>
-            <p>Cilj nije "odradivanje terapije", nego stvaran oporavak.</p>
+            <p>
+              Individualan pristup, jasna dijagnostika i terapija bez “šablona”. 
+              <br/>Cilj nije “odrađivanje terapije”, nego stvaran oporavak.
+            </p>
             <p>U Kinetisu terapija započinje razumijevanjem problema. Svaki pacijent prolazi klinički pregled na temelju kojeg se donosi odluka što ima smisla raditi, a što ne.</p>
           </div>
         </div>
